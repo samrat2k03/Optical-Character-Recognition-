64411faa4931580eb45982479040b8b896d75952
Samrat D <samratapr40@gmail.com>
2023-12-31 19:56:38 +0530

    responsive and loader

--- ocr.js
+++ ocr.js
@@ -6,7 +6,7 @@ const ocr = express.Router();
 ocr.post('/', async(req,res) => {
     
     const { file } = req;
-    console.log('Received file:', file)
+    // console.log('Received file:', file)
 
     if (!file) {
         return res.status(400).send('Source image not provided.');
@@ -17,7 +17,7 @@ ocr.post('/', async(req,res) => {
     Tesseract.recognize(source, "eng")
     .then(({ data: { text } }) => { 
         res.status(200).send(text);
-        console.log(text)
+        // console.log(text)
      }
         )
     .catch((err) => {
